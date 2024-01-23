a6d1ef2537913ae5492f454e918760cfb75cbadb
Vanessa Tang <vanesst5@uci.edu>
2024-01-22 17:49:51 -0800

    Adjusted useEffect/added more functionality ------------------------ Still need testing

--- frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
+++ frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
@@ -6,83 +6,83 @@ import apiClient from '../../Services/apiClient'
 import { Dialog, DialogContent, DialogTitle, DialogActions, Input } from '@mui/material'
 import { useAuthContext } from '../../Services/authProvider'
 
-const test_comments = [
-  {
-    username: 'user1',
-    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    likes: 4
-  },
-  {
-    username: 'user2',
-    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    likes: 6
-  },
-  {
-    username: 'user3',
-    comment: 'comment3',
-    likes: 5
-  },
-  {
-    username: 'user4',
-    comment: 'comment4',
-    likes: 9
-  },
-  {
-    username: 'user5',
-    comment: 'comment5',
-    likes: 0
-  },
-  {
-    username: 'user1',
-    comment: 'comment1',
-    likes: 2
-  },
-  {
-    username: 'user2',
-    comment: 'comment2',
-    likes: 8
-  },
-  {
-    username: 'user3',
-    comment: 'comment3',
-    likes: 0
-  },
-  {
-    username: 'user4',
-    comment: 'comment4',
-    likes: 0
-  },
-  {
-    username: 'user5',
-    comment: 'comment5',
-    likes: 0
-  },
-  {
-    username: 'user1',
-    comment: 'comment1',
-    likes: 0
-  },
-  {
-    username: 'user2',
-    comment: 'comment2',
-    likes: 0
-  },
-  {
-    username: 'user3',
-    comment: 'comment3',
-    likes: 0
-  },
-  {
-    username: 'user4',
-    comment: 'comment4',
-    likes: 57
-  },
-  {
-    username: 'user5',
-    comment: 'comment5',
-    likes: 467
-  }
-]
+// const test_comments = [
+//   {
+//     username: 'user1',
+//     comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+//     likes: 4
+//   },
+//   {
+//     username: 'user2',
+//     comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+//     likes: 6
+//   },
+//   {
+//     username: 'user3',
+//     comment: 'comment3',
+//     likes: 5
+//   },
+//   {
+//     username: 'user4',
+//     comment: 'comment4',
+//     likes: 9
+//   },
+//   {
+//     username: 'user5',
+//     comment: 'comment5',
+//     likes: 0
+//   },
+//   {
+//     username: 'user1',
+//     comment: 'comment1',
+//     likes: 2
+//   },
+//   {
+//     username: 'user2',
+//     comment: 'comment2',
+//     likes: 8
+//   },
+//   {
+//     username: 'user3',
+//     comment: 'comment3',
+//     likes: 0
+//   },
+//   {
+//     username: 'user4',
+//     comment: 'comment4',
+//     likes: 0
+//   },
+//   {
+//     username: 'user5',
+//     comment: 'comment5',
+//     likes: 0
+//   },
+//   {
+//     username: 'user1',
+//     comment: 'comment1',
+//     likes: 0
+//   },
+//   {
+//     username: 'user2',
+//     comment: 'comment2',
+//     likes: 0
+//   },
+//   {
+//     username: 'user3',
+//     comment: 'comment3',
+//     likes: 0
+//   },
+//   {
+//     username: 'user4',
+//     comment: 'comment4',
+//     likes: 57
+//   },
+//   {
+//     username: 'user5',
+//     comment: 'comment5',
+//     likes: 467
+//   }
+// ]
 
 function ViewAllCommentsPopup ( { postId } ) {
   const [open, setOpen] = useState(false)
@@ -91,11 +91,26 @@ function ViewAllCommentsPopup ( { postId } ) {
   const { userId, username } = user
   const [comments, setComments] = useState([])
   
-  function getPostComments(postId) {
+  // function getPostComments(postId) {
+  //   apiClient.getPostComments(postId).then(res => {
+  //     setComments(res.data)
+  //   });
+  // }
+  // const getPostComments = async () => {
+  //   try {
+  //       let response = await apiClient.getPostComments(postId)
+  //       let comments = response.data;
+  //       setComments(comments)
+  //   } catch (error) {
+  //       console.error('Error fetching comments:', error);
+  //   }
+  // }
+
+  useEffect((postId) => {
     apiClient.getPostComments(postId).then(res => {
       setComments(res.data)
-    });
-  }
+    })
+  }, [])
 
 
   function handleToggle() {
@@ -106,18 +121,18 @@ function ViewAllCommentsPopup ( { postId } ) {
   function handleAddNewComment (event) {
     if(event.keyCode === 13 && !event.shiftKey) {
       event.preventDefault()
-      console.log('Comment ', newComment)
+      // console.log('Comment ', newComment)
       // // call api to add comment
-      // apiClient.addComment(userId, postId, newComment)
-      test_comments.push({ username: username, comment: newComment, likes: 0 })
+      apiClient.addComment(userId, postId, newComment)
+      // test_comments.push({ username: username, comment: newComment, likes: 0 })
       setNewComment('')
     }
   }
 
-  useEffect((getPostComments, postId)=>{
-    // setComments(test_comments)
-    getPostComments(postId)
-  }, [])
+  // useEffect((getPostComments, postId)=>{
+  //   // setComments(test_comments)
+  //   getPostComments(postId)
+  // }, [])
 
   return (
     <div>
