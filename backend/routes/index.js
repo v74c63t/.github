21026576249ac3bb9d76f98e195731cc85cac00a
Vanessa Tang <vanesst5@uci.edu>
2024-01-22 20:59:11 -0800

    Added post routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.use('/', require('./auth/authIndex'))
 router.use('/users', require('./user/userIndex'))
+router.use('/', require('./post/postIndex'))
 
 module.exports = router
